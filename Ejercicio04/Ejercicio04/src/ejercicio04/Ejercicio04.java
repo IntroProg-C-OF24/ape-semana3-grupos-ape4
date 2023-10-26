4e0a5643994cd06ca5c9459bfc85d0475072a15e
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2023-10-26 17:40:23 -0500

    Ejercicio04 RicardoE

---
+++ Ejercicio04/Ejercicio04/src/ejercicio04/Ejercicio04.java
@@ -0,0 +1,12 @@
+package ejercicio04;
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        int var1 = 20;
+        int var2 = 1;
+        int var3 = 3;
+        int var4 = 2;
+        int resultado1 = (var1/(var2 + var3))+2;
+        boolean resultadofinal = resultado1 >= var3;
+        System.out.println(resultadofinal);
+    }
+}
