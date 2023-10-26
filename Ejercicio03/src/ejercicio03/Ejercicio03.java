9ab9ecfce9a15f0c04c72557caff70bc232340aa
2p4n <jdguerrero021422@gmail.com>
2023-10-26 17:47:21 -0500

    Ejercicio03 JD

--- Ejercicio03/src/ejercicio03/Ejercicio03.java
+++ Ejercicio03/src/ejercicio03/Ejercicio03.java
@@ -1,22 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio03;
-
-/**
- *
- * @author reroes
- */
 public class Ejercicio03 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+        int var1 = 10;
+        int var2 = 16;
+        double var3 = 20;
+        int var4 = 1;
+        int var5 = 11;
+        double respuesta;
+        respuesta = Math.pow(var1, 2) + Math.sqrt(var2) + var3 / (var4 + var5) ;
+        System.out.println("La respuesta es" + " " + respuesta);
     }
     
 }
