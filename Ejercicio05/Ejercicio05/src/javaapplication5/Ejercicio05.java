01a96cebc2e4a077d8e32b3e9ac9b9e0fe2ca0bf
2p4n <jdguerrero021422@gmail.com>
2023-10-27 14:21:10 -0500

    Ejercicio05 JD

---
+++ Ejercicio05/Ejercicio05/src/javaapplication5/Ejercicio05.java
@@ -0,0 +1,13 @@
+package javaapplication5;
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        int var1 = 2;
+        int var2 = 12;
+        int var3 = 1;
+        int var4 = 3;
+        double respuesta;
+        respuesta = (Math.pow(var1, 2) + var2 - var3)/ 3;
+        System.out.println("La respuesta es igual a: " + respuesta);
+    }
+    
+}
