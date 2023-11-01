62aaaa7cde1616b7804a0f5e92d04115647f0270
Ricardo Espinosa <rfespinosa7@utpl.edu.ec>
2023-10-31 21:47:48 -0500

    Ejercicio07 JD

--- Ejercicio07/Ejercicio07/src/ejercicio07/Ejercicio07.java
+++ Ejercicio07/Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -9,7 +9,7 @@ public class Ejercicio07 {
         double var6 = 100;
         double var7 = 25;
             boolean respuesta01;
-                respuesta01 = ((Math.sqrt(var1) + var2)/var3 == var2) || (var4 > var5) && (var6/var7 + Math.sqrt(var6));
+                respuesta01 = ((Math.sqrt(var1) + var2)/var3 == var2) || (var4 > var5) && (var6/var7 + Math.sqrt(var6)) == 0;
         System.out.println(respuesta01);
     }
         
